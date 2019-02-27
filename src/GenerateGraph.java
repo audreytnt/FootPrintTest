d57d28a4e691ba61bad4f2bf9fe310050f62ebf5
Audrey Tran-Thien <atranthien@gmail.com>
2019-02-26 20:06:50 -0800

    added jars and generate graph

---
+++ src/GenerateGraph.java
@@ -0,0 +1,63 @@
+import com.lowagie.text.Document;
+import com.lowagie.text.pdf.DefaultFontMapper;
+import com.lowagie.text.pdf.PdfContentByte;
+import com.lowagie.text.pdf.PdfTemplate;
+import com.lowagie.text.pdf.PdfWriter;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.io.FileOutputStream;
+
+public class GenerateGraph {
+    public static void main(String[] args) {
+        writeChartToPDF(generateBarChart(), 500, 400, "./barchart.pdf");
+    }
+
+    public static void writeChartToPDF(JFreeChart chart, int width, int height, String fileName) {
+        PdfWriter writer = null;
+
+        Document document = new Document();
+
+        try {
+            writer = PdfWriter.getInstance(document, new FileOutputStream(
+                    fileName));
+            document.open();
+            PdfContentByte contentByte = writer.getDirectContent();
+            PdfTemplate template = contentByte.createTemplate(width, height);
+            Graphics2D graphics2d = template.createGraphics(width, height,
+                    new DefaultFontMapper());
+            Rectangle2D rectangle2d = new Rectangle2D.Double(0, 0, width,
+                    height);
+
+            chart.draw(graphics2d, rectangle2d);
+
+            graphics2d.dispose();
+            contentByte.addTemplate(template, 0, 0);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        document.close();
+    }
+
+
+    public static JFreeChart generateBarChart() {
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        dataSet.setValue(791, "Population", "1750 AD");
+        dataSet.setValue(978, "Population", "1800 AD");
+        dataSet.setValue(1262, "Population", "1850 AD");
+        dataSet.setValue(1650, "Population", "1900 AD");
+        dataSet.setValue(2519, "Population", "1950 AD");
+        dataSet.setValue(6070, "Population", "2000 AD");
+
+        JFreeChart chart = ChartFactory.createBarChart(
+                "World Population growth", "Year", "Population in millions",
+                dataSet, PlotOrientation.VERTICAL, false, true, false);
+
+        return chart;
+    }
+}
